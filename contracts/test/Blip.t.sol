35aed5a86a0ffe9d96c259010f82a9f16113cfe1
vita-tak <isacbroman@gmail.com>
2025-10-17 12:59:23 +0200

    test test

--- contracts/test/Blip.t.sol
+++ contracts/test/Blip.t.sol
@@ -11,26 +11,58 @@ contract BlipTest is Test {
     address public Alice = vm.addr(129);
 
     function setUp() public {
-        kontrakt = new Blip();
+        kontrakt = new Blip(address(this));
         vm.deal(Alice, 1 ether);
     }
 
-    function test_initPayment() public {
-
-        uint balance = Alice.balance;
-        assertEq (balance, 1 ether);
-
-        kontrakt.addGuardian(address(0x0000000000000000000000000000000000000000));
-
-        vm.prank(Alice);
-
-        kontrakt.initPayment{
-            value: 1 * 10**18
-        }();
-
-        uint newBalance = Alice.balance;
-        assertEq (newBalance, 0);
+    function test_recipientIsSetCorrectly() public view {
+        address recipient = kontrakt.recipientAddress();
+        assertEq(recipient, address(this));
     }
+
+    function test_proposeGuardian() public {
+        kontrakt.proposeGuardian(Alice); 
+        bool isPending = kontrakt.pendingGuardians(Alice);
+        assertEq(isPending, true);
+    }
+
+    function test_cannotProposeZeroAddress() public {
+        vm.expectRevert(Blip.InvalidAddress.selector);
+        kontrakt.proposeGuardian(address(0));
+    }
+
+    function test_recipientCannotBeGuardian() public {
+        vm.expectRevert(Blip.RecipientCannotBeGuardian.selector);
+        kontrakt.proposeGuardian(address(this));
+    }
+
+    function test_acceptGuardianRole() public {
+        // 1. Recipient föreslår Alice
+        kontrakt.proposeGuardian(Alice);
+        // 2. Alice accepterar (använd vm.prank!)
+        vm.prank(Alice);
+        kontrakt.acceptGuardianRole();
+        // 3. Verifiera att Alice är aktiv guardian
+        assertEq(kontrakt.pendingGuardians(Alice), false);
+        assertEq(kontrakt.guardiansMap(Alice), true);
+    }
+
+    // function test_initPayment() public {
+
+    //     uint balance = Alice.balance;
+    //     assertEq (balance, 1 ether);
+
+    //     kontrakt.addGuardian(address(0x0000000000000000000000000000000000000000));
+
+    //     vm.prank(Alice);
+
+    //     kontrakt.initPayment{
+    //         value: 1 * 10**18
+    //     }();
+
+    //     uint newBalance = Alice.balance;
+    //     assertEq (newBalance, 0);
+    // }
 
     // function test_Increment() public {
     //     address sender = msg.sender;
