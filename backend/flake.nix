3459fd42c077e0a03f86dd0feaa38ffafba99c97
Reblixt <carl@klofverskjold.se>
2025-10-28 10:44:01 +0100

    added flake

---
+++ backend/flake.nix
@@ -0,0 +1,14 @@
+{
+  description = "Development environment";
+
+  inputs = { nixpkgs = { url = "github:NixOS/nixpkgs/nixos-unstable"; }; };
+
+  outputs = { self, nixpkgs }:
+    let
+      system = builtins.currentSystem or "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      devShells.${system}.default =
+        pkgs.mkShell { packages = with pkgs; [ nest-cli ]; };
+    };
+}
