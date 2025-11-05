994e6e25927b1f49faea0b7a0fe5b5a7c8895fc1
vita-tak <isacbroman@gmail.com>
2025-11-05 19:42:10 +0100

    Add GuardianRemoved listener

--- backend/src/blockchain/blockchain.service.ts
+++ backend/src/blockchain/blockchain.service.ts
@@ -165,6 +165,36 @@ export class BlockchainService implements OnModuleInit {
         );
       },
     });
+
+    this.viem.watchContractEvent({
+      abi: blipAbi,
+      address: '0x5FbDB2315678afecb367f032d93F642f64180aa3' as Address,
+      eventName: 'GuardianRemoved',
+      onLogs: (log) => {
+        const recipient = log[0].args.recipientAddress;
+        const guardian = log[0].args.guardianAddress;
+
+        this.logger.debug(
+          `GuardianRemoved event detected: Recipient - ${JSON.stringify(
+            recipient,
+            (key, value) =>
+              typeof value === 'bigint' ? value.toString() : value,
+            2
+          )}, Guardian - ${JSON.stringify(
+            guardian,
+            (key, value) =>
+              typeof value === 'bigint' ? value.toString() : value,
+            2
+          )}`
+        );
+      },
+
+      onError: (error) => {
+        this.logger.error(
+          `Error watching GuardianRemoved events: ${error.message}`
+        );
+      },
+    });
   }
 
   async watchPaymentEvents() {
