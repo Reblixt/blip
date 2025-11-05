3b64faa11141c996dd6573ae001560ef0f00c3b4
vita-tak <isacbroman@gmail.com>
2025-11-05 19:45:23 +0100

    Add removeGuardian script

--- contracts/script/removeGuardian.s.sol
+++ contracts/script/removeGuardian.s.sol
@@ -10,7 +10,7 @@ contract CounterScript is Script {
     //forge script script/Blip.s.sol --rpc-url http://127.0.0.1:8545 --broadcast
 
     uint256 PRIVATE_KEY = 0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80;
-    address newGuardian = 0x70997970C51812dc3A010C7d01b50e0d17dc79C8;
+    address guardianToRemove = 0x70997970C51812dc3A010C7d01b50e0d17dc79C8;
 
     Blip public blip;
 
@@ -22,7 +22,7 @@ contract CounterScript is Script {
         vm.startBroadcast(PRIVATE_KEY);
 
         blip = Blip(payable(address(0x5FbDB2315678afecb367f032d93F642f64180aa3)));
-        blip.proposeGuardian(newGuardian);
+        blip.removeGuardian(guardianToRemove);
         console.log("Owner/sender", msg.sender);
         console.log("contract address", address(blip));
 
