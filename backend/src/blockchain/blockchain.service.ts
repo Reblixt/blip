9ebdc40edf489b9030ef3bad2e3260322b838276
vita-tak <isacbroman@gmail.com>
2025-11-05 19:35:44 +0100

    Add GuardianLeftRole listener

--- backend/src/blockchain/blockchain.service.ts
+++ backend/src/blockchain/blockchain.service.ts
@@ -135,6 +135,36 @@ export class BlockchainService implements OnModuleInit {
         );
       },
     });
+
+    this.viem.watchContractEvent({
+      abi: blipAbi,
+      address: '0x5FbDB2315678afecb367f032d93F642f64180aa3' as Address,
+      eventName: 'GuardianLeftRole',
+      onLogs: (log) => {
+        const recipient = log[0].args.recipient;
+        const guardian = log[0].args.guardian;
+
+        this.logger.debug(
+          `GuardianLeftRole event detected: Recipient - ${JSON.stringify(
+            recipient,
+            (key, value) =>
+              typeof value === 'bigint' ? value.toString() : value,
+            2
+          )}, Guardian - ${JSON.stringify(
+            guardian,
+            (key, value) =>
+              typeof value === 'bigint' ? value.toString() : value,
+            2
+          )}`
+        );
+      },
+
+      onError: (error) => {
+        this.logger.error(
+          `Error watching GuardianLeftRole events: ${error.message}`
+        );
+      },
+    });
   }
 
   async watchPaymentEvents() {
