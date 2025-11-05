ec4310dae799c87fdf4c6df7ad43f1e93e5a8c55
vita-tak <isacbroman@gmail.com>
2025-11-05 19:18:19 +0100

    Add declineGuardian script

--- contracts/script/declineGuardian.s.sol
+++ contracts/script/declineGuardian.s.sol
@@ -9,8 +9,7 @@ contract CounterScript is Script {
     //NOTE:
     //forge script script/Blip.s.sol --rpc-url http://127.0.0.1:8545 --broadcast
 
-    uint256 PRIVATE_KEY = 0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80;
-    address newGuardian = 0x3C44CdDdB6a900fa2b585dd299e03d12FA4293BC;
+    uint256 PRIVATE_KEY = 0x5de4111afa1a4b94908f83103eb1f1706367c2e68ca870fc3fb9a804cdab365a;
 
     Blip public blip;
 
@@ -22,7 +21,7 @@ contract CounterScript is Script {
         vm.startBroadcast(PRIVATE_KEY);
 
         blip = Blip(payable(address(0x5FbDB2315678afecb367f032d93F642f64180aa3)));
-        blip.proposeGuardian(newGuardian);
+        blip.declineGuardianRole();
         console.log("Owner/sender", msg.sender);
         console.log("contract address", address(blip));
 
