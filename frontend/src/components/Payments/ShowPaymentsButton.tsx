bc3955ab107fbae79580aa446b3da5d01ba48864
vita-tak <isacbroman@gmail.com>
2025-11-16 23:51:05 +0100

    update show payments button UI

--- frontend/src/components/Payments/ShowPaymentsButton.tsx
+++ frontend/src/components/Payments/ShowPaymentsButton.tsx
@@ -1,27 +1,34 @@
+import { History } from 'lucide-react';
+
 interface ShowHistoryButtonProps {
   onClick: () => void;
 }
 
 export function ShowPaymentsButton({ onClick }: ShowHistoryButtonProps) {
   return (
-    <button
-      onClick={onClick}
-      className='      w-full sm:w-auto
-      bg-blue-500
-      hover:bg-blue-600
-      text-white
-      px-6
-      py-4 sm:py-3
-      text-lg sm:text-sm
-      rounded-xl
-      shadow-lg
-      flex
-      items-center
-      justify-center
-      gap-2
-      transition-colors
-      duration-200'>
-      Visa historik
-    </button>
+    <div className='fixed bottom-24 left-0 right-0 flex justify-center z-40 px-4'>
+      <button
+        onClick={onClick}
+        className='
+          w-full sm:w-auto
+          bg-blue-500
+          hover:bg-blue-600
+          text-white
+          px-6
+          py-4 sm:py-3
+          text-lg sm:text-sm
+          rounded-xl
+          shadow-lg
+          flex
+          items-center
+          justify-center
+          gap-2
+          transition-colors
+          duration-200
+        '>
+        <History size={24} />
+        <span className='font-semibold'>Show payments</span>
+      </button>
+    </div>
   );
 }
